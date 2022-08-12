a84f07474ea8718385c5dda988111fd27c7a95f5
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-08-12 16:31:28 +0100

    Remove unneeded Config type

--- lab_share_lib/config_readers.py
+++ lab_share_lib/config_readers.py
@@ -1,13 +1,14 @@
 from lab_share_lib.rabbit.schema_registry import SchemaRegistry
-from lab_share_lib.types import RabbitServerDetails, Config
+from lab_share_lib.types import RabbitServerDetails
 from lab_share_lib.rabbit.basic_publisher import BasicPublisher
 import sys
 import os
-from typing import cast, Tuple
+from typing import Tuple
 from importlib import import_module
+from types import ModuleType
 
 
-def get_config(settings_module: str = "") -> Tuple[Config, str]:
+def get_config(settings_module: str = "") -> Tuple[ModuleType, str]:
     """Get the config for the app by importing a module named by an environment variable. This allows easy switching
     between environments and inheriting default config values.
 
@@ -22,20 +23,20 @@ def get_config(settings_module: str = "") -> Tuple[Config, str]:
         if not settings_module:
             settings_module = os.environ["SETTINGS_MODULE"]
 
-        config_module = cast(Config, import_module(settings_module))
+        config_module = import_module(settings_module)
 
         return config_module, settings_module
     except KeyError as e:
         sys.exit(f"{e} required in environment variables for config.")
 
 
-def get_redpanda_schema_registry(config: Config) -> SchemaRegistry:
+def get_redpanda_schema_registry(config: ModuleType) -> SchemaRegistry:
     redpanda_url = config.REDPANDA_BASE_URI
     redpanda_api_key = config.REDPANDA_API_KEY
     return SchemaRegistry(redpanda_url, redpanda_api_key)
 
 
-def get_rabbit_server_details(config: Config, username: str = "", password: str = "") -> RabbitServerDetails:
+def get_rabbit_server_details(config: ModuleType, username: str = "", password: str = "") -> RabbitServerDetails:
     return RabbitServerDetails(
         uses_ssl=config.RABBITMQ_SSL,
         host=config.RABBITMQ_HOST,
@@ -46,7 +47,7 @@ def get_rabbit_server_details(config: Config, username: str = "", password: str 
     )
 
 
-def get_basic_publisher(config: Config, username: str = "", password: str = "") -> BasicPublisher:
+def get_basic_publisher(config: ModuleType, username: str = "", password: str = "") -> BasicPublisher:
     return BasicPublisher(
         get_rabbit_server_details(config, username, password),
         config.RABBITMQ_PUBLISH_RETRY_DELAY,
