1dde3101b6e113affc6c8409b04528b8d9bf08e9
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-02-03 11:33:07 +0000

    Testing dependency updates

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Library to allow creating consumers to interact with lab-share framework",
     author="Stuart McHattie",
     license="MIT",
-    install_requires=["pika", "fastavro~=1.5", "requests"],
+    #install_requires=["pika~=1.3", "fastavro~=1.5", "requests~=1.10"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     test_suite="tests",
