5660527c2e9469a5a611239533ec17df9136709e
Stuart McHattie <sm49@sanger.ac.uk>
2024-05-01 18:49:48 +0100

    Use new class names in the example scripts

--- examples/example1/processors.py
+++ examples/example1/processors.py
@@ -3,7 +3,7 @@ import datetime
 
 class Example1MessageProcessor:
     def __init__(self, schema_registry, basic_publisher, config):
-        return
+        pass
 
     def process(self, message):
         print(f"Message read from the queue at { datetime.datetime.now() }:")
