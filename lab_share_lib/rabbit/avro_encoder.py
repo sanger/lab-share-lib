1dc4c047202999fcbf67acfd7a64ea9761168e79
Stuart McHattie <sm49@sanger.ac.uk>
2024-05-01 12:47:44 +0100

    Add a single-object binary Avro encoder

--- lab_share_lib/rabbit/avro_encoder.py
+++ lab_share_lib/rabbit/avro_encoder.py
@@ -99,6 +99,63 @@ class AvroEncoderBinaryFile(AvroEncoderBase):
         return fastavro.reader(bytes_reader, self._schema(schema_response))
 
 
+class AvroEncoderBinaryMessage(AvroEncoderBase):
+    """An encoder for single-object Avro messages.
+    This sort of encoding reduces the overhead of the file encoding by removing the schema from the content of the
+    message. This is suitable for sending and receiving messages via a message broker where the schema is stored in a
+    schema registry. The schema used for writing is identified by a 64-bit CRC fingerprint of the schema. Note however,
+    the fingerprint may be inconsistent between different Avro implementations, so it cannot be relied upon. Therefore
+    it is recommended to indicate the schema subject and version in the message metadata.
+
+    Raises:
+        ValueError: If the number of records is not exactly 1 while encoding.
+        ValueError: If the message binary does not start with the expected two-byte marker.
+    """
+
+    TWO_BYTE_MARKER = b"\xC3\x01"  # Used to identify single-object Avro encoding.
+
+    def __init__(self, schema_registry, subject):
+        super().__init__(schema_registry, subject)
+
+    def _schema_fingerprint(self, schema_response):
+        canonical_form = fastavro.schema.to_parsing_canonical_form(self._schema(schema_response))
+        return fastavro.schema.fingerprint(canonical_form, "CRC-64-AVRO")
+
+    def encode(self, records: List, version: Optional[str] = None) -> EncodedMessage:
+        if records is None or len(records) != 1:
+            raise ValueError("AvroEncoderBinaryMessage only supports encoding exactly one object at a time.")
+
+        return self.encode_single_object(records[0], version)
+
+    def encode_single_object(self, record: Any, version: Optional[str] = None) -> EncodedMessage:
+        LOGGER.debug("Encoding AVRO message.")
+
+        schema_response = self._schema_response(version)
+
+        bytes_writer = BytesIO()
+        bytes_writer.write(self.TWO_BYTE_MARKER)
+        bytes_writer.write(bytes.fromhex(self._schema_fingerprint(schema_response)))
+        fastavro.schemaless_writer(bytes_writer, self._schema(schema_response), record)
+
+        return EncodedMessage(body=bytes_writer.getvalue(), version=str(self._schema_version(schema_response)))
+
+    def decode(self, message: bytes, version: str) -> Any:
+        LOGGER.debug("Decoding AVRO message.")
+
+        schema_response = self._schema_response(version)
+        bytes_reader = BytesIO(message)
+
+        marker = bytes_reader.read(2)
+        if marker != self.TWO_BYTE_MARKER:
+            raise ValueError("Message does not appear to be a single-object Avro encoding.")
+
+        # Skip the schema fingerprint as our schema registry doesn't support looking up by fingerprint.
+        bytes_reader.seek(10)
+
+        # There's something wrong with mypy linting for the number of arguments on this method!
+        return fastavro.schemaless_reader(bytes_reader, self._schema(schema_response))  # type: ignore[call-arg]
+
+
 class AvroEncoderBinary(AvroEncoderBinaryFile):
     """Included for backwards compatibility. This class is now an alias for AvroEncoderBinaryFile."""
 
