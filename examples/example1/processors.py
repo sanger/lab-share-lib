0ac1c06bca0a364ebd28cca8264dcb2978313018
Stuart McHattie <sm49@sanger.ac.uk>
2024-05-08 16:26:22 +0100

    Use BaseProcessor in example processor class

--- examples/example1/processors.py
+++ examples/example1/processors.py
@@ -1,9 +1,11 @@
 import datetime
 
+from lab_share_lib.processing.base_processor import BaseProcessor
 
-class Example1MessageProcessor:
-    def __init__(self, schema_registry, basic_publisher, config):
-        pass
+
+class Example1MessageProcessor(BaseProcessor):
+    def instantiate(self, schema_registry, basic_publisher, config):
+        return Example1MessageProcessor()
 
     def process(self, message):
         print(f"Message read from the queue at { datetime.datetime.now() }:")
